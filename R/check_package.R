d62961742ee6f4985e306e0b5346191a6a68417e
Domingo Lopez <dominlopez78@gmail.com>
2017-11-29 14:03:55 +0100

    Added function to check packages

---
+++ R/check_package.R
@@ -0,0 +1,35 @@
+check_package <- function(path = ".", 
+                          test_coverage = TRUE,
+                          type = c("test", "vignette", "example",
+                                   "all", "none")) {
+  
+  safe_require("devtools")
+  safe_require("testthat")
+  
+  if (test_coverage) {
+    
+    safe_require("covr")
+    
+    cov <- package_coverage(path = path, type = type[1])
+    
+    csv <- file.path(path, "tests", "test_results.csv")
+    
+    df <- read.delim(csv, sep = " ")
+    
+  } else {
+    
+    df <- devtools::test(path)
+    
+  }
+  
+  cat("Test results:\n")
+  print(df)
+  
+  if (test_coverage) {
+    
+    cat("\nOpening coverage")
+    shine(cov)
+    
+  }
+  
+}
